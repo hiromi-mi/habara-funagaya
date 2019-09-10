8ab54ea82f1d604c0a2b936e063cf70562e27a42
hiromi-mi <hiromi-mi@cat.zaq.jp>
2019-09-11 01:21:36 +0900

    support unregister

--- main.go
+++ main.go
@@ -10,13 +10,13 @@ import (
 
 type Event struct {
 	Title   string
-	Members []string
+	Members map[string]string
 }
 
 var events = make(map[string]Event)
 
 var templates = template.Must(template.ParseFiles("vote.html"))
-var validPath = regexp.MustCompile("^/(events|new|register)/([a-zA-Z0-9]+)$")
+var validPath = regexp.MustCompile("^/(events|new|register|unregister)/([a-zA-Z0-9]+)$")
 
 func eventshandler(w http.ResponseWriter, r *http.Request) {
 	m := validPath.FindStringSubmatch(r.URL.Path)
@@ -45,15 +45,31 @@ func registershandler(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	event.Members = append(event.Members, "hiromi_mi")
+	event.Members["hiromi_mi"] = "aaa"
+	events[m[2]] = event
+	http.Redirect(w, r, "/events/"+m[2], http.StatusFound)
+}
+func unregistershandler(w http.ResponseWriter, r *http.Request) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, r)
+		return
+	}
+	event, ok := events[m[2]]
+	if !ok {
+		http.NotFound(w, r)
+		return
+	}
+	delete(event.Members, "hiromi_mi")
 	events[m[2]] = event
 	http.Redirect(w, r, "/events/"+m[2], http.StatusFound)
 }
 
 func main() {
-	p := Event{Title: "TestEvent", Members: []string{"alice", "bob"}}
+	p := Event{Title: "TestEvent", Members: make(map[string]string)}
 	events[p.Title] = p
 	http.HandleFunc("/events/", eventshandler)
 	http.HandleFunc("/register/", registershandler)
+	http.HandleFunc("/unregister/", unregistershandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
